9da7506e3af4745692ba43a40d846be4a7b612f6
Matheus Silva <matheus.silva50@estudante.ifms.edu.br>
2026-09-07 17:57:18 +0000

    trasicao-logiin

---
+++ front/script.js
@@ -0,0 +1,16 @@
+const navs = document.querySelectorAll('.btn-nav');
+
+navs.forEach((nav) => nav.addEventListener('click', () => navClicked(nav)));
+
+const navClicked = (nav) => {
+  const contents = document.querySelectorAll('.content');
+
+  contents.forEach((content) => content.classList.remove('active'));
+
+  const contentId = nav.getAttribute('content-id');
+  const content = document.getElementById(contentId);
+
+  if (content) {
+    content.classList.add('active');
+  }
+};
